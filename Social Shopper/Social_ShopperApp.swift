df875ce183a9b6f897ea987bf72697a38a0052b0
Min Lee <minwoolee@gmail.com>
2025-04-01 13:39:11 -0700

    Initial commit - Gemini Canvas generated

---
+++ Social Shopper/Social_ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_ShopperApp.swift
+//  Social Shopper
+//
+//  Created by Min Woo Lee on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Social_ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Social ShopperTests/Social_ShopperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Social_ShopperTests.swift
+//  Social ShopperTests
+//
+//  Created by Min Woo Lee on 4/1/25.
+//
+
+import Testing
+@testable import Social_Shopper
+
+struct Social_ShopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
